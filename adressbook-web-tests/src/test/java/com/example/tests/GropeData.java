4fc835457d88e7e9112df897ba1c639cb81fd9c6
KasyanenkoOI <severtko@mail.ru>
2020-09-08 13:49:17 +0300

    Добавлен тест с рекордера, рефакторинг сделан

--- adressbook-web-tests/src/test/java/com/example/tests/GropeData.java
+++ adressbook-web-tests/src/test/java/com/example/tests/GropeData.java
@@ -1,11 +1,11 @@
 package com.example.tests;
 
-public class GropData {
+public class GropeData {
     private final String name;
     private final String headerName;
     private final String footerName;
 
-    public GropData(String name, String headerName, String footerName) {
+    public GropeData(String name, String headerName, String footerName) {
         this.name = name;
         this.headerName = headerName;
         this.footerName = footerName;
